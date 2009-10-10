58bf8adfe011fd636a3291cc45061bad62102130
dlukyanov <dlukyanov@ukr.net>
2009-10-10 20:48:08 +0000

    comment folding

--- dllsrc/scintilla/src/b.bat
+++ dllsrc/scintilla/src/b.bat
@@ -1,9 +1,9 @@
 @echo off
-set path=c:\HOME\VC\bin;%path%
-set include=c:\HOME\VC\include
-set lib=c:\HOME\VC\lib
+set path=d:\HOME\VC\bin;%path%
+set include=d:\HOME\VC\include
+set lib=d:\HOME\VC\lib
 cd ..\win32
 
 nmake -f scintilla_vc6.mak %1 %2 %3
 
-copy ..\bin\SciLexer.dll ..\..\
+copy /Y ..\bin\SciLexer.dll ..\..\..\dll
